036cf2c50162a2a0217e9b7a30e49d248bdb71e9
Mary Koenke <mkoenke@sas.upenn.edu>
2020-09-16 14:40:01 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,7 +5,7 @@ low_temps =[]
 outer_index = 0
 while outer_index < src.count do
   inner_index = 0
-  lowest_temp = 1000
+  lowest_temp = [outer_index][0]
   while inner_index < src[outer_index].count do
     if src[outer_index][inner_index] < lowest_temp
       lowest_temp = src[outer_index][inner_index]
